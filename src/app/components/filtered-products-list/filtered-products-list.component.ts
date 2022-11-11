961485452615a2bd5883d44ef56aeff9a45ec5b3
MrMizus <jankowski.michal96@gmail.com>
2022-11-11 13:03:01 +0100

    feat: added filtered products list based on subject

--- src/app/components/filtered-products-list/filtered-products-list.component.ts
+++ src/app/components/filtered-products-list/filtered-products-list.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
-import {Observable, combineLatest, map} from 'rxjs';
-import {ProductsModel} from '../../models/products.model';
-import {CategoriesService} from '../../services/categories.service';
-import {ProductService} from '../../services/product.service';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ProductsModel } from '../../models/products.model';
+import { CategoriesService } from '../../services/categories.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-filtered-products-list',
@@ -13,13 +14,19 @@ import {ProductService} from '../../services/product.service';
 })
 export class FilteredProductsListComponent {
   readonly categories$: Observable<string[]> = this._categoriesService.getAll();
+  private _categorySubject: Subject<string> = new Subject<string>();
+  public category$: Observable<string> = this._categorySubject.asObservable();
   readonly products$: Observable<ProductsModel[]> = combineLatest([
     this._productService.getAll(),
-    this._activatedRoute.params
-  ]).pipe(map(([products, params]: [ProductsModel[], Params]) => {
-    return products.filter((product: ProductsModel) => product.category === params['category']);
-  }))
+    this.category$
+  ]).pipe(map(([products, category]: [ProductsModel[], string]) => {
+    return products.filter(product => product.category === category);
+  }));
 
   constructor(private _categoriesService: CategoriesService, private _productService: ProductService, private _activatedRoute: ActivatedRoute) {
+  }
+
+  selectCategory(category: string): void {
+    this._categorySubject.next(category);
   }
 }
