545e9b9d6c38bb99971bc97c43d06cbf12e6a712
MrMizus <jankowski.michal96@gmail.com>
2022-11-11 13:30:58 +0100

    feat: added sorted products list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FilteredProductsListComponent } from './components/filtered-products-list/filtered-products-list.component';
+import { SortedProductListComponent } from './components/sorted-product-list/sorted-product-list.component';
 import { FilteredProductsListComponentModule } from './components/filtered-products-list/filtered-products-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { ProductServiceModule } from './services/product.service-module';
+import { SortedProductListComponentModule } from './components/sorted-product-list/sorted-product-list.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products/:category', component: FilteredProductsListComponent }]), FilteredProductsListComponentModule, CategoriesServiceModule, ProductServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products/:category', component: FilteredProductsListComponent }, { path: 'sorted-products', component: SortedProductListComponent }]), FilteredProductsListComponentModule, CategoriesServiceModule, ProductServiceModule, SortedProductListComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/sorted-product-list/sorted-product-list.component-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { SortedProductListComponent } from './sorted-product-list.component';
+import {FlexModule} from "@angular/flex-layout";
+
+@NgModule({
+  imports: [MatCardModule, MatListModule, CommonModule, FlexModule],
+  declarations: [SortedProductListComponent],
+  providers: [],
+  exports: [SortedProductListComponent]
+})
+export class SortedProductListComponentModule {
+}
