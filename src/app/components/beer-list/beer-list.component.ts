bfcd952a3db82af75e8df8de14ddb921d7ff190b
MrMizus <jankowski.michal96@gmail.com>
2022-11-22 10:08:14 +0100

    feat: added beer lis with pagination

--- src/app/components/beer-list/beer-list.component.ts
+++ src/app/components/beer-list/beer-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { BeerModel } from '../../models/beer.model';
 import { BeerListService } from '../../services/beer-list.service';
 import { PageEvent } from '@angular/material/paginator';
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-beer-list',
@@ -11,11 +12,18 @@ import { PageEvent } from '@angular/material/paginator';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BeerListComponent {
-  list$: Observable<BeerModel[]> = this._beerListService.getAll();
+  public pagination = [1, 5]
+  private _refreshSubject: BehaviorSubject<void> = new BehaviorSubject<void>(void 0);
+  public refresh$: Observable<void> = this._refreshSubject.asObservable();
+  readonly refreshedList$: Observable<BeerModel[]> = this.refresh$.pipe(
+    switchMap(data => this._beerListService.getPage(this.pagination[0], this.pagination[1])));
 
   constructor(private _beerListService: BeerListService) {
   }
 
   OnPageChange($event: PageEvent) {
+    this.pagination[0] = $event.pageIndex + 1
+    this.pagination[1] = $event.pageSize
+    this._refreshSubject.next();
   }
 }
