013d5c5183a5e7dfed280534d0aad128ee3113fa
MrMizus <jankowski.michal96@gmail.com>
2022-11-24 14:12:46 +0100

    feat: added product search list

---
+++ src/app/components/products-search/products-search.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, combineLatest, debounceTime} from 'rxjs';
+import { ProductsModel } from '../../models/products.model';
+import { ProductService } from '../../services/product.service';
+import {map} from "rxjs/operators";
+
+@Component({
+  selector: 'app-products-search',
+  templateUrl: './products-search.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductsSearchComponent {
+  readonly search: FormGroup = new FormGroup({ title: new FormControl() });
+  readonly startsWith$: Observable<string> = this.search.valueChanges.pipe(
+    map(form => form.title),
+    debounceTime(1000)
+  );
+
+  readonly list$: Observable<ProductsModel[]> = combineLatest([
+    this._productService.getAll(),
+    this.startsWith$,
+  ]).pipe(
+    map(([products, startWith]) => {
+      if (!startWith) {
+        return [];
+      }
+      return products.filter(product => product.title.startsWith(startWith))
+    })
+  )
+
+  constructor(private _productService: ProductService) {
+  }
+
+}
