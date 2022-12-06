a8cb4c8c34dc9faa2245fdd22d03b59a9e159ab9
MrMizus <jankowski.michal96@gmail.com>
2022-12-06 09:25:41 +0100

    feat: crypto autocomplete done

--- src/app/components/crypto-autocomplete/crypto-autocomplete.component-module.ts
+++ src/app/components/crypto-autocomplete/crypto-autocomplete.component-module.ts
@@ -7,10 +7,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
 import { CryptoAutocompleteComponent } from './crypto-autocomplete.component';
 
 @NgModule({
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatAutocompleteModule, MatOptionModule, CommonModule, MatButtonModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatAutocompleteModule, MatOptionModule, CommonModule, MatButtonModule, MatListModule, MatChipsModule],
   declarations: [CryptoAutocompleteComponent],
   providers: [],
   exports: [CryptoAutocompleteComponent]
--- src/app/components/crypto-autocomplete/crypto-autocomplete.component.ts
+++ src/app/components/crypto-autocomplete/crypto-autocomplete.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Observable, combineLatest } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, debounceTime } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CryptoModel } from '../../models/crypto.model';
 import { CryptoService } from '../../services/crypto.service';
@@ -12,24 +12,29 @@ import { CryptoService } from '../../services/crypto.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CryptoAutocompleteComponent {
+  public array: string[] = []
   readonly search: FormGroup = new FormGroup({ crypto: new FormControl() });
   readonly startsWith$: Observable<string> = this.search.valueChanges.pipe(
     map(form => {
-      console.log(form.symbol)
-      return form.symbol
+      return form.crypto.toUpperCase()
     }),
+    debounceTime(1000)
   );
 
   readonly crypto$: Observable<CryptoModel[]> = combineLatest([
     this._cryptoService.getAll(),
     this.startsWith$,
   ]).pipe(
-    map(([crypto, startWith]) => {
-      return crypto.filter(crypto => crypto.symbol.startsWith(startWith))
-    })
+    map(([crypto, startWith]) => crypto.filter(crypto => crypto.symbol.startsWith(startWith)))
   )
+  private _chipSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+  public chip$: Observable<string[]> = this._chipSubject.asObservable();
+
+  addChip(crypto: string): void {
+    this.array.push(crypto)
+    this._chipSubject.next(this.array)
+  }
 
   constructor(private _cryptoService: CryptoService) {
   }
-
 }
