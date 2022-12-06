3a99f3bbf5e8e40ae837f2f55326d7cb579f0df0
Mizus <jankowski.michal96@gmail.com>
2022-12-06 18:49:04 +0100

    changed job-form

--- src/app/components/job-form/job-form.component.ts
+++ src/app/components/job-form/job-form.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
-import {Observable} from 'rxjs';
+import {Observable, tap} from 'rxjs';
 import {JobTagsModel} from '../../models/job-tags.model';
 import {JobTagsService} from '../../services/job-tags.service';
 import {JobPostsService} from '../../services/job-posts.service';
@@ -12,32 +12,37 @@ import {JobPostsService} from '../../services/job-posts.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JobFormComponent {
-  public array: number[]  = [];
-  readonly tags$: Observable<JobTagsModel[]> = this._jobTagsService.getAll();
+  readonly tags$: Observable<JobTagsModel[]> = this._jobTagsService.getAll().pipe(
+    tap(data => this.setControls(data))
+  )
+  readonly jobTagsIdForm: FormGroup = new FormGroup({})
   readonly jobForm: FormGroup = new FormGroup({
     title: new FormControl(),
     description: new FormControl(),
-    tags: new FormControl()
+    jobTagsIdForm: this.jobTagsIdForm
   });
+
 
   constructor(private _jobTagsService: JobTagsService, private _jobPostsService: JobPostsService) {
   }
 
-  onChange(event: any) {
-    if (event.checked){
-      this.array.push(parseInt(event.source.value))
-    } else {
-      const i = this.array.findIndex(x => x === event.source.value);
-      this.array.splice(i, 1)
-    }
+  setControls(jobTagIds: JobTagsModel[]): void {
+    jobTagIds.forEach(
+      tag => this.jobTagsIdForm.addControl(
+        tag.id, new FormControl(false)
+      )
+    )
   }
 
 
   onJobFormSubmitted(jobForm: FormGroup): void {
+    console.log(this.jobTagsIdForm)
     this._jobPostsService.create({
       title: jobForm.get("title")?.value,
       description: jobForm.get("description")?.value,
-      jobTagIds: this.array
+      jobTagIds: Object.keys(this.jobTagsIdForm.value).filter((key) => {
+        return this.jobTagsIdForm.value[key] === true
+      }).map(tag => +tag)
     }).subscribe();
   }
 }
